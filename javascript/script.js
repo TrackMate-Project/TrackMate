00b8b47637c5469cb4ad6638af86b3cf1a52eb28
Katie <katie.mcy@outlook.com>
2023-03-03 13:59:24 -0500

    started on colorLegend javascript

--- javascript/script.js
+++ javascript/script.js
@@ -7,15 +7,17 @@ const dbRef = ref(database);
 const colorRef = ref(database, 'category')
 // console.log(colorRef);
 
-const color1But = document.querySelector('#color1But');
-const color2But = document.querySelector('#color2But');
-const color3But = document.querySelector('#color3But');
+// const color1But = document.querySelector('#color1But');
+// const color2But = document.querySelector('#color2But');
+// const color3But = document.querySelector('#color3But');
+
+const colorButtons = document.querySelectorAll('.colorLegendBut');
 
 const color1Input = document.querySelector('#color1');
 const color2Input = document.querySelector('#color2');
 const color3Input = document.querySelector('#color3');
 
-const color1Obj = colorRef.color1;
+const categoryRef = ref(database, `/category`);
 
 // color1But.addEventListener('click', function(){
 //     // .preventDefault();
@@ -24,13 +26,37 @@ const color1Obj = colorRef.color1;
     
 //     if (colorInput !== ""){
 //         const description = {
-//             color1: `colorInput`
+//             color1: `${colorInput}`
 //         }
-//         update(color1Obj, description);
+//         console.log(description);
+//         update(categoryRef, description);
 //     } else {
 //         alert('Please enter a valid description! Do not leave the input empty')
 //     }
 // })
+
+colorButtons.forEach(function(individualButton){
+    individualButton.addEventListener('click', function(){
+        const buttonValue = this.value;
+        console.log(typeof buttonValue);
+        console.log(buttonValue);
+
+        const colorInput = color1Input.value.trim();
+    
+        if (colorInput !== ""){ 
+            console.log(buttonValue);
+        
+            const description = {
+                buttonValue : `${colorInput}`
+            };
+            console.log(description);
+            update(categoryRef, description);
+            
+        } else {
+            alert('Please enter a valid description! Do not leave the input empty')
+        }
+        })
+})
 
 // Pseudocode for Color Category
 // Define variable for button, and user input
